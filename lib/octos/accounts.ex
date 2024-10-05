2fab5810ea772a33d994e7e8148f86921f3bafcc
Joel Jucá <joeljuca@proton.me>
2024-10-05 16:00:20 -0300

    feat: impl sorting asc/desc by name for `Accounts.list_users/1`

--- lib/octos/accounts.ex
+++ lib/octos/accounts.ex
@@ -8,13 +8,22 @@ defmodule Octos.Accounts do
   alias Octos.Cameras
   alias Octos.Repo
 
-  @spec list_users() ::
+  @spec list_users(params :: map()) ::
           {:ok, list(Accounts.User.t())} | {:error, term()}
-  def list_users() do
+  def list_users(%{} = params \\ %{}) do
+    order_by = Map.get(params, "order_by")
+    order_by = if order_by not in ["name"], do: nil, else: String.to_atom(order_by)
+    order = Map.get(params, "order")
+    order = if order not in ["asc", "desc"], do: :asc, else: String.to_atom(order)
+
     query =
       from(u in Accounts.User)
       |> join(:left, [u], c in Cameras.Camera, on: u.id == c.user_id)
       |> preload([u], [:cameras])
+      |> distinct(true)
+      |> then(fn query ->
+        if is_nil(order_by), do: query, else: query |> order_by([u], [{^order, ^order_by}])
+      end)
 
     {:ok, Repo.all(query)}
   rescue
--- test/octos/accounts_test.exs
+++ test/octos/accounts_test.exs
@@ -1,7 +1,44 @@
 defmodule Octos.AccountsTest do
   use Octos.DataCase
+  import Octos.Factory
+  alias Octos.Accounts
 
-  describe "Accounts ctx" do
-    # TODO
+  setup do
+    for m <- [Octos.Cameras.Camera, Octos.Accounts.User], do: Octos.Repo.delete_all(m)
+
+    insert(:user)
+    |> then(&insert_list(2, :camera, user_id: &1.id))
+
+    :ok
+  end
+
+  describe "Accounts.list_users/1" do
+    test "lists users with their cameras" do
+      assert {:ok, users} = Accounts.list_users()
+      assert is_list(users)
+      for user <- users, do: assert(%Octos.Accounts.User{} = user)
+    end
+
+    test "allows ordering by name (asc)" do
+      insert(:user)
+
+      user_names = Octos.Accounts.User.all() |> Enum.map(& &1.name)
+
+      assert {:ok, users} = Accounts.list_users(%{"order_by" => "name"})
+      assert is_list(users)
+
+      assert user_names == for(u <- users, do: u.name)
+    end
+
+    test "allows ordering by name (desc)" do
+      insert(:user)
+
+      user_names = Octos.Accounts.User.all() |> Enum.map(& &1.name) |> Enum.sort(:desc)
+
+      assert {:ok, users} = Accounts.list_users(%{"order_by" => "name", "order" => "desc"})
+      assert is_list(users)
+
+      assert user_names == for(u <- users, do: u.name)
+    end
   end
 end
