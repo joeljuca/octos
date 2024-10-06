6cf8acdca2a2879e23e16a9f5adb8a081d060d5f
Joel Jucá <joeljuca@proton.me>
2024-10-06 13:59:44 -0300

    fix: Gettext warning over deprecated `use Gettext`
    ```
    warning: defining a Gettext backend by calling
    
        use Gettext, otp_app: ...
    
    is deprecated. To define a backend, call:
    
        use Gettext.Backend, otp_app: :my_app
    
    Then, instead of importing your backend, call this in your module:
    
        use Gettext, backend: MyApp.Gettext
    
      lib/web/gettext.ex:23: OctosWeb.Gettext (module)
    ```

--- lib/web/gettext.ex
+++ lib/web/gettext.ex
@@ -20,5 +20,5 @@ defmodule OctosWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :octos
+  use Gettext.Backend, otp_app: :octos
 end
